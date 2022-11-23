d00958d5cec82b3d2403b8ccd1298eaae586d128
Peter Rodgers <P.J.Rodgers@kent.ac.uk>
2022-11-23 15:36:15 +0000

    deleted eclipse files

--- src/module-info.java
+++ src/module-info.java
@@ -5,12 +5,5 @@
  * @author pjr
  *
  */
- 
- 
 module embedder {
-	requires java.desktop;
-	requires org.jgrapht.core;
-	requires org.jgrapht.ext;
-	requires org.jgrapht.io;
-	requires com.github.vlsi.mxgraph.jgraphx;
 }
